d292903a03753bb478b6c332a1fddb599b0ea479
Phuong Tran <phuongtrantran0973@gmail.com>
2023-05-30 17:00:39 +0700

    Apply BasePage finish

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -16,6 +16,9 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
+	public static BasePage getBasePageObject() {
+		return new BasePage();
+	}
     public void openURL(WebDriver driver, String pageUrl) {
     	driver.get(pageUrl);
 	}
