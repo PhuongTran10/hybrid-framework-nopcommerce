cf1b50bfc4f58b8438da5971e17f917ce6cde231
Tran Thi Bich Phuong <phuongtrantran0973@gmail.com>
2023-05-30 07:27:15 +0700

    apply BasePage I

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -97,15 +97,15 @@ public class BasePage {
 		}
 	}
 	
-	public By getByXpath(String xpathLocator) {
+	private By getByXpath(String xpathLocator) {
 		return By.xpath(xpathLocator);
 	}
 	
-	public WebElement getWebElement(WebDriver driver, String xpathLocator) {
+	private WebElement getWebElement(WebDriver driver, String xpathLocator) {
 		return driver.findElement(getByXpath(xpathLocator));
 	}
 	
-	public List<WebElement> getListWebElement(WebDriver driver, String xpathLocator) {
+	private List<WebElement> getListWebElement(WebDriver driver, String xpathLocator) {
 		return driver.findElements(getByXpath(xpathLocator));
 	}
 	
