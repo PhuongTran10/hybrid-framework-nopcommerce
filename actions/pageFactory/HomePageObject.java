bb2ed4a4d84ef0411fca53c1b48ecede2f8fabb8
Tran Thi Bich Phuong <phuongtrantran0973@gmail.com>
2023-06-12 07:38:35 +0700

    Page Factory

---
+++ actions/pageFactory/HomePageObject.java
@@ -0,0 +1,10 @@
+package pageFactory;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePageObject {
+	private WebDriver driver;
+	public HomePageObject(WebDriver driver) {
+		this.driver = driver;
+	}
+}
