1f1459155f7fef991d1894d125946250979f0f53
Tran Thi Bich Phuong <phuongtrantran0973@gmail.com>
2023-06-28 23:07:14 +0700

    Add file testNG sources.jar

---
+++ javaTester/javaBasic/Topic_02_Assert_And_Verify.java
@@ -0,0 +1,52 @@
+package javaBasic;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import commons.BaseTest;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@Listeners(commons.MethodListener.class)
+public class Topic_02_Assert_And_Verify extends BaseTest{
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+	}
+
+	@Test
+	public void TC_01_ValidateCurrentUrl() {
+		System.out.println("Assert 01");
+		String loginPageUrl = driver.getCurrentUrl();
+		verifyEquals(loginPageUrl, "https://www.facebook.com/");
+		System.out.println("Assert 02");
+		String loginPageTitle = driver.getTitle();
+		verifyEquals(loginPageTitle, "Facebook – log in or sign up");
+		System.out.println("Assert 03");
+		verifyFalse(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
+		System.out.println("Assert 04");
+		verifyFalse(driver.findElement(By.xpath("//input[@name='jazoest']")).isDisplayed());
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
